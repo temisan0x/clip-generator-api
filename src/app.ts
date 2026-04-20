34bc480e93db38ef9e35d55980b1debc13fef8e2
temycodes <temycodes@gmail.com>
2026-04-20 21:31:01 +0100

    build: code cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,22 @@
 import express from "express";
 import cors from "cors";
 import { createFileRouter } from "./routes/fileRoutes";
-import rateLimit from "express-rate-limit";
-
-// const globalLimiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   max: 100,
-//   message: { error: "Too many requests from this IP." },
-// });
 
 const app = express();
 
+app.set("trust proxy", 1); 
+
 app.use(cors({
-  origin: "http://localhost:3001",
+  origin: ["https://clipora.vercel.app", "http://localhost:3001"],
   methods: ["GET", "POST"],
 }));
 
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ extended: true, limit: "200mb", parameterLimit: 100000 }));
-// app.use(globalLimiter);
 app.use("/api", createFileRouter());
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
-app.set("trust proxy", 1)
 
 export default app;
